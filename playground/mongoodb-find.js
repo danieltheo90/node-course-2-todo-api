3ae8feb3079910e9ec4d1ee97a1d2982028aa520
danieltheo90 <dtheo90@yahoo.com>
2018-03-06 21:37:20 +0700

    First Commit

---
+++ playground/mongoodb-find.js
@@ -0,0 +1,28 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// var user ={name :'Daniel',age:25};
+// var {name} = user;
+
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=>{
+	if (err){
+		console.log('Unable to connect to MongoDb server');
+	}
+	console.log('connected to mongoodbServer');
+
+	// db.collection('Todos').find().count().then((count)=>{
+	// 	console.log(`Todos:${count}`);
+	// 	//console.log(JSON.stringify(docs,undefined,2));
+	// },(err)=>{
+	// 	console.log('Unable to fetch todos',err);
+	// });
+	db.collection('Users').find({name:'Daniel'}).toArray().then((docs)=>{
+		console.log(JSON.stringify(docs, undefined,2));
+	});
+	db.close();
+});
